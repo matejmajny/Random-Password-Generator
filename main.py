ac6970fafa5fb4aa7db3d12e1ccd11cdf335da7f
Andrew <66240551+dumpydev@users.noreply.github.com>
2022-05-16 20:33:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -10,34 +10,25 @@ symbols = input("Do you want special characters? ")
 numbers = input("Do you want numbers? ")
 capital = input("Do you want capital letters? ")
 print("\n\n")
-
-
 letters = shuffle("qwertyuiopasdfghjklzxcvbnm")
-
-#capitals
+# capitals
 if capital == "yes":
     capital_letters = shuffle("QWERTYUIOPASDFGHJKLZXCVBNM")
 else:
     capital_letters = ""
-
-#special
+# special
 if symbols == "yes":
     special = shuffle("!@#$%^&*()_+{}[]<>?/|")
 else:
     special = ""
-
-#numbers
+# numbers
 if numbers == "yes":
     numbers1 = shuffle("1234567890")
 else:
     numbers1 = ""
-
-long = int(long)
 final = shuffle(capital_letters + special + numbers1 + letters)
-
 print(Fore.GREEN + "Your generated password is: " + final + Fore.BLUE)
 copy = input("Do you want copy your password? ")
-
 if copy == "yes":
     pyperclip.copy(final)
 
