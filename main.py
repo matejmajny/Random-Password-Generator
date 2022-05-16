531ee598b9c9f3551d607318d404053e9bcab9f5
Andrew <66240551+dumpydev@users.noreply.github.com>
2022-05-16 21:15:04 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
-import string_utils
 from colorama import *
 import random
 import pyperclip
 init()
-
-long = input("How long should your password be? ")
-symbols = input("Do you want special symobls?")
+def shuffle(chars):
+    return ''.join(random.sample(chars, len(chars)))
+print(Fore.RED)
+long = input("How many characters do you want your password to be? ")
+symbols = input("Do you want special characters? ")
 numbers = input("Do you want numbers? ")
 capital = input("Do you want capital letters? ")
 print("\n\n")
-
-
-letters = string_utils.shuffle("qwertyuiopasdfghjklzxcvbnm")
-
-#capitals
+letters = shuffle("qwertyuiopasdfghjklzxcvbnm")
+# capitals
 if capital == "yes":
-    capital_letters = string_utils.shuffle("QWERTYUIOPASDFGHJKLZXCVBNM")
+    capital_letters = shuffle("QWERTYUIOPASDFGHJKLZXCVBNM")
 else:
     capital_letters = ""
-
-#special
+# special
 if symbols == "yes":
-    special = string_utils.shuffle("&@&{Łßß÷")
+    special = shuffle("!@#$%^&*()_+{}[]<>?/|")
 else:
     special = ""
-
-#numbers
+# numbers
 if numbers == "yes":
-    numbers1 = string_utils.shuffle("1234567890")
+    numbers1 = shuffle("1234567890")
 else:
     numbers1 = ""
-
-long = int(long)
-final = string_utils.shuffle(capital_letters + special + numbers1 + letters)
-final = final[:long]
-
+final = shuffle(capital_letters + special + numbers1 + letters)
 print(Fore.GREEN + "Your generated password is: " + final + Fore.BLUE)
 copy = input("Do you want copy your password? ")
-
 if copy == "yes":
     pyperclip.copy(final)
 
